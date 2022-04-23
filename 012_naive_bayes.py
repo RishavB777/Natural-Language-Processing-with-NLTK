deea35e6ed47c85966c5eda135bd09044a10f0d1
RISHAV BHATTACHARJEE <rishavbhattacharjee777@gmail.com>
2022-04-23 12:44:06 +0530

    Add files via upload

---
+++ 012_naive_bayes.py
@@ -0,0 +1,44 @@
+import nltk
+import random
+from nltk.corpus import movie_reviews
+
+documents = [(list(movie_reviews.words(fileid)), category) 
+                        for category in movie_reviews.categories()
+                        for fileid in movie_reviews.fileids(category)]
+docs = []
+for category in movie_reviews.categories():
+    for fileid in movie_reviews.fileids(category):
+        docs.append((list(movie_reviews.words(fileid)), category))
+
+random.shuffle(documents)
+
+#print(documents[1]) positive
+
+all_words = []
+for w in movie_reviews.words():
+    all_words.append(w.lower())
+
+all_words = nltk.FreqDist(all_words)
+
+word_features = list(all_words.keys())[:3000]
+
+def find_features(doc):
+    features = {}
+    words = set(doc)
+    for w in word_features:
+        features[w] = (w in words)
+    return features
+
+featuresets = [(find_features(rev),category) for (rev,category) in documents]
+#print(featuresets)
+"""for (rev,category) in documents[:1]:
+    print(f"{rev},{category}")
+"""
+
+training_set = featuresets[:1900]
+test_set = featuresets[1900:]
+
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+print("Naive Bayes Accuracy:", (nltk.classify.accuracy(classifier,test_set))*100)
+classifier.show_most_informative_features(15)
